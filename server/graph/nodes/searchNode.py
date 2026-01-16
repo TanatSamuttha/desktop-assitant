1b7ed925aed72cc1599e582cdeebc5000fffc919
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2026-01-16 23:54:23 +0700

    Fix search node

--- server/graph/nodes/searchNode.py
+++ server/graph/nodes/searchNode.py
@@ -1,6 +1,6 @@
 import webbrowser
 from models.pydantic.chatModel import ChatState
 
-async def search_node():
-    url = ""
+async def search_node(state: ChatState) -> ChatState:
+    url = ChatState["link"]
     webbrowser.open(url)
