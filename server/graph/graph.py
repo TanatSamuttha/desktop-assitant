cbc98569ac16787decced26c306b89c64fc7f98a
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2026-01-16 23:47:58 +0700

    graph

--- server/graph/graph.py
+++ server/graph/graph.py
@@ -2,12 +2,13 @@ from langgraph.graph import StateGraph,START,END
 
 from models.pydantic.chatModel import ChatState
 from graph.nodes.chatNode import chat_node
+from graph.nodes.searchNode import search_node
 
 builder = StateGraph(ChatState)
 builder.add_node("chat", chat_node)
-builder.add_node("search", )
+builder.add_node("search", search_node)
 builder.add_edge(START, "chat")
-builder.add_edge("chat", "spliter")
-builder.add_edge("spliter", END)
+builder.add_edge("chat", "search")
+builder.add_edge("search", END)
 
 graph = builder.compile()
