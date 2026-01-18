f51f16209a89d71068009cfa2113e18cccecd9f1
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2026-01-18 23:47:04 +0700

    new prompt

--- server/graph/nodes/splitNode.py
+++ server/graph/nodes/splitNode.py
@@ -1,7 +1,7 @@
 from models.pydantic.chatModel import ChatState
 
 async def split_node(state: ChatState) -> ChatState:
-    task, url = state.raw_response.split()
+    task, url = state.raw_response.split("-<([split])>-")
     return {
         "user_message": state.user_message,
         "task": task,
